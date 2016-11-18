154ecbd1fcdcf5a709299459cff9223bac966d2c
own3mall <own3mall@gmail.com>
2016-11-17 19:14:10 -0700

    rocco's language changes

--- lang/English/modules/billing.php
+++ lang/English/modules/billing.php
@@ -21,10 +21,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-#####################################################################
-# English language variables
-#
-##################################################################### 
 
 define('currency', "Currency");
 define('available_invoice_types', "Available Invoice Types");
