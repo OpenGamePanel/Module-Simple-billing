de5ce30c4774875b57d3acbeef64f1425811a8c1
own3mall <own3mall@gmail.com>
2017-04-20 16:44:59 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Italian/modules/simple-billing.php
@@ -0,0 +1,153 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2016 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('currency', "Currency");
+define('available_invoice_types', "Available Invoice Types");
+define('hourly', "Hourly");
+define('monthly', "Monthly");
+define('annually', "Annually");
+define('tax_amount', "Tax Amount");
+define('paypal_email', "PayPal E-mail");
+define('update_settings', "Update Settings");
+define('settings_updated', "Settings Updated.");
+define('payment_gateway', "Payment Gateway");
+define('paygol_service', "PayGol Service");
+define('paygol_service_id', "PayGol Service ID");
+define('paypal', "PayPal");
+define('paygol', "PayGol");
+define('skrill', "Skrill");
+define('skrill_merchant_info', "Skrill merchant information");
+define('skrill_merchant_id', "Merchant ID.");
+define('skrill_email', "Skrill Email");
+define('skrill_secret_word', "Secret word");
+define('skrill_secret_word_info', "The password must be set from your Skrill merchant account,<br>at <b>Settings</b> click on <b>Developer Settings</b>.<br>If this option is not listed send an email to <i>merchantservices@skrill.com</i>.");
+define('hash_stored_correctly', "Hash stored correctly");
+define('currency_not_available_at', "The currency \"%s\" is not available at %s.");
+define('robokassa', "Robokassa");
+define('robokassa_service', "Robokassa service");
+define('robokassa_merchant_login', "Merchant Login");
+define('robokassa_securepass1', "Secure Password 1");
+define('robokassa_securepass2', "Secure Password 2");
+define('your_cart', "Your Cart");
+define('starting_on', "Starting on");
+define('slots', "Slots");
+define('hour', "Hour");
+define('month', "Month");
+define('year', "Year");
+define('hours', "Hours");
+define('months', "Months");
+define('years', "Years");
+define('service_name', "Service name");
+define('rcon_pass', "RCON password");
+define('ftp_pass', "FTP password");
+define('available_ips', "Available IPs");
+define('max_players', "Max Players");
+define('invoice_duration', "Invoice Duration");
+define('calculate_price', "Calculate Price");
+define('buy', "BUY");
+define('back_to_list', "Back to the list");
+define('ip', "IP");
+define('subtotal', "Subtotal");
+define('rate', "Rate");
+define('total', "Total");
+define('save', "Save");
+define('you_need_to', "You need to");
+define('register', "Register");
+define('and', "and");
+define('log_in', "Log In");
+define('to_purchase_a_service', "to purchase a service");
+define('available_services', "Available Services");
+define('payment_is_pending_of_approval', "The payment is pending of approval.");
+define('back_to_your_cart', "Go back to your cart.");
+define('expired', "Expired");
+define('removed', "Removed");
+define('extended', "Extended");
+define('installation_and_expiration_date', "Installation and expiration date");
+define('expiration_date', "Expiration date");
+define('removal_date', "Removal date");
+define('installation_date', "Installation date");
+define('enable_server', "Enable server");
+define('success', "SUCCESS");
+define('redirecting_to_game_monitor', "Redirecting to Game Monitor...");
+define('starting_installations', "Starting installations...");
+define('order_id', "Order ID");
+define('home_name', "Home Name");
+define('tax', "Tax");
+define('pay_from', "Pay From %s");
+define('set_as_paid', "Set As Paid");
+define('create_server', "Create Server");
+define('see_invoice', "See Invoice");
+define('paid', "Paid");
+define('not_paid', "Not Paid");
+define('procesing_payment', "Processing Payment");
+define('paid_and_installed', "Paid And Installed");
+define('add_to_cart', "Add To Cart");
+define('cart_id', "Cart ID");
+define('order_desc', "Order Description");
+define('remove_from_cart', "Remove From Cart");
+define('remove_cart', "Remove Cart");
+define('there_are_no_orders_in_cart', "There are no orders in this cart.");
+define('redirecting_to', "Redirecting to %s...");
+define('id', "ID#");
+define('remote_server', "Remote Server");
+define('price_hourly', "Price Hourly");
+define('price_monthly', "Price Monthly");
+define('price_year', "Price Year");
+define('service_image_url', "Service Image URL");
+define('remove_service', "Remove Service");
+define('add_service', "Add Service");
+define('current_services', "Current Services");
+define('max_slot_qty', "Max. Slot Quantity");
+define('min_slot_qty', "Min. Slot Quantity");
+define('ftp_account', "FTP Account");
+define('select_install_method', "Select install method");
+define('url_for_manual_install', "URL For Manual Install");
+define('description', "Description");
+define('image_url', "Image URL");
+define('access_rights', "Access Rights");
+define('allow_update', "Allow Update");
+define('allow_file_management', "Allow File Management");
+define('allow_parameter_usage', "Allow Parameter Usage");
+define('allow_extra_parameters_usage', "Allow Extra Parameters Usage");
+define('allow_ftp_usage', "Allow FTP Usage");
+define('allow_custom_fields', "Allow Custom Fields");
+define('enabled', "Enabled");
+define('disabled', "Disabled");
+define('steam', "Steam");
+define('rsync', "Rsync");
+define('manual_from_url', "Manual From URL");
+define('business', "Business");
+define('business_email', "Business Email");
+define('game_server_order', "Game Server Order");
+define('item', "Item");
+define('slot_cost', "Slot Cost");
+define('slot_quantity', "Slot Quantity");
+define('order_price', "Order Price");
+define('order', "Order");
+define('date', "Date");
+define('price', "Price");
+define('invoice', "Invoice");
+define('print_invoice', "Print Invoice");
+define('extend', "Extend");
+?>
