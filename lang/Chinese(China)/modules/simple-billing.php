a769eeb75e2eaf24773395cba1c045a78867cc03
own3mall <own3mall@gmail.com>
2024-02-26 21:21:39 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Chinese(China)/modules/simple-billing.php
@@ -0,0 +1,153 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_currency', "货币");
+define('OGP_LANG_available_invoice_types', "可用的发票类型");
+define('OGP_LANG_hourly', "每小时");
+define('OGP_LANG_monthly', "每月");
+define('OGP_LANG_annually', "每年");
+define('OGP_LANG_tax_amount', "税额");
+define('OGP_LANG_paypal_email', "PayPal 电子邮件");
+define('OGP_LANG_update_settings', "更新设置");
+define('OGP_LANG_settings_updated', "设置已更新。");
+define('OGP_LANG_payment_gateway', "支付网关");
+define('OGP_LANG_paygol_service', "PayGol 服务");
+define('OGP_LANG_paygol_service_id', "PayGol 服务 ID");
+define('OGP_LANG_paypal', "PayPal");
+define('OGP_LANG_paygol', "PayGol");
+define('OGP_LANG_skrill', "Skrill");
+define('OGP_LANG_skrill_merchant_info', "Skrill 商户信息");
+define('OGP_LANG_skrill_merchant_id', "商户 ID");
+define('OGP_LANG_skrill_email', "Skrill 电子邮件");
+define('OGP_LANG_skrill_secret_word', "Skrill密钥");
+define('OGP_LANG_skrill_secret_word_info', "密码必须从您的 Skrill 商户账户设置中设置，<br>在 <b>设置</b> 中点击 <b>开发者设置</b>。<br>如果此选项未列出，请发送电子邮件至 <i>merchantservices@skrill.com</i>。");
+define('OGP_LANG_hash_stored_correctly', "哈希值正确存储");
+define('OGP_LANG_currency_not_available_at', "货币 \"%s\" 在 %s 不可用。");
+define('OGP_LANG_robokassa', "Robokassa");
+define('OGP_LANG_robokassa_service', "Robokassa 服务");
+define('OGP_LANG_robokassa_merchant_login', "商户登录");
+define('OGP_LANG_robokassa_securepass1', "安全密码 1");
+define('OGP_LANG_robokassa_securepass2', "安全密码 2");
+define('OGP_LANG_your_cart', "您的购物车");
+define('OGP_LANG_starting_on', "开始于");
+define('OGP_LANG_slots', "玩家人数");
+define('OGP_LANG_hour', "小时");
+define('OGP_LANG_month', "月");
+define('OGP_LANG_year', "年");
+define('OGP_LANG_hours', "小时数");
+define('OGP_LANG_months', "月数");
+define('OGP_LANG_years', "年数");
+define('OGP_LANG_service_name', "服务名称");
+define('OGP_LANG_rcon_pass', "RCON 密码");
+define('OGP_LANG_ftp_pass', "FTP 密码");
+define('OGP_LANG_available_ips', "可用的 IP");
+define('OGP_LANG_max_players', "最大玩家数");
+define('OGP_LANG_invoice_duration', "发票期限");
+define('OGP_LANG_calculate_price', "计算价格");
+define('OGP_LANG_buy', "购买");
+define('OGP_LANG_back_to_list', "返回列表");
+define('OGP_LANG_ip', "IP");
+define('OGP_LANG_subtotal', "小计");
+define('OGP_LANG_rate', "费率");
+define('OGP_LANG_total', "总计");
+define('OGP_LANG_save', "保存");
+define('OGP_LANG_you_need_to', "您需要");
+define('OGP_LANG_register', "注册");
+define('OGP_LANG_and', "和");
+define('OGP_LANG_log_in', "登录");
+define('OGP_LANG_to_purchase_a_service', "购买服务");
+define('OGP_LANG_available_services', "可用的服务");
+define('OGP_LANG_payment_is_pending_of_approval', "支付待审批。");
+define('OGP_LANG_back_to_your_cart', "返回您的购物车。");
+define('OGP_LANG_expired', "已过期");
+define('OGP_LANG_removed', "已移除");
+define('OGP_LANG_extended', "已延长");
+define('OGP_LANG_installation_and_expiration_date', "安装和到期日期");
+define('OGP_LANG_expiration_date', "到期日期");
+define('OGP_LANG_removal_date', "移除日期");
+define('OGP_LANG_installation_date', "安装日期");
+define('OGP_LANG_enable_server', "启用服务器");
+define('OGP_LANG_success', "成功");
+define('OGP_LANG_redirecting_to_game_monitor', "重定向到游戏监视器...");
+define('OGP_LANG_starting_installations', "开始安装...");
+define('OGP_LANG_order_id', "订单 ID");
+define('OGP_LANG_home_name', "主页名称");
+define('OGP_LANG_tax', "税");
+define('OGP_LANG_pay_from', "支付来源 %s");
+define('OGP_LANG_set_as_paid', "设为已支付");
+define('OGP_LANG_create_server', "创建服务器");
+define('OGP_LANG_see_invoice', "查看发票");
+define('OGP_LANG_paid', "已支付");
+define('OGP_LANG_not_paid', "未支付");
+define('OGP_LANG_procesing_payment', "处理支付中");
+define('OGP_LANG_paid_and_installed', "已支付并已安装");
+define('OGP_LANG_add_to_cart', "添加到购物车");
+define('OGP_LANG_cart_id', "购物车 ID");
+define('OGP_LANG_order_desc', "订单描述");
+define('OGP_LANG_remove_from_cart', "从购物车中移除");
+define('OGP_LANG_remove_cart', "移除购物车");
+define('OGP_LANG_there_are_no_orders_in_cart', "购物车中没有订单。");
+define('OGP_LANG_redirecting_to', "重定向到 %s...");
+define('OGP_LANG_id', "ID#");
+define('OGP_LANG_remote_server', "远程服务器");
+define('OGP_LANG_price_hourly', "价格 每小时");
+define('OGP_LANG_price_monthly', "价格 每月");
+define('OGP_LANG_price_year', "价格 每年");
+define('OGP_LANG_service_image_url', "服务图片 URL");
+define('OGP_LANG_remove_service', "移除服务");
+define('OGP_LANG_add_service', "添加服务");
+define('OGP_LANG_current_services', "当前服务");
+define('OGP_LANG_max_slot_qty', "最大玩家人数量");
+define('OGP_LANG_min_slot_qty', "最小玩家人数量");
+define('OGP_LANG_ftp_account', "FTP 账户");
+define('OGP_LANG_select_install_method', "选择安装方法");
+define('OGP_LANG_url_for_manual_install', "手动安装的 URL");
+define('OGP_LANG_description', "描述");
+define('OGP_LANG_image_url', "图片 URL");
+define('OGP_LANG_access_rights', "访问权限");
+define('OGP_LANG_allow_update', "允许更新");
+define('OGP_LANG_allow_file_management', "允许文件管理");
+define('OGP_LANG_allow_parameter_usage', "允许参数使用");
+define('OGP_LANG_allow_extra_parameters_usage', "允许额外参数使用");
+define('OGP_LANG_allow_ftp_usage', "允许 FTP 使用");
+define('OGP_LANG_allow_custom_fields', "允许自定义字段");
+define('OGP_LANG_enabled', "已启用");
+define('OGP_LANG_disabled', "已禁用");
+define('OGP_LANG_steam', "Steam");
+define('OGP_LANG_rsync', "Rsync");
+define('OGP_LANG_manual_from_url', "来自 URL 的手动");
+define('OGP_LANG_business', "商业");
+define('OGP_LANG_business_email', "商业电子邮件");
+define('OGP_LANG_game_server_order', "游戏服务器订单");
+define('OGP_LANG_item', "项目");
+define('OGP_LANG_slot_cost', "玩家人数成本");
+define('OGP_LANG_slot_quantity', "玩家人数量");
+define('OGP_LANG_order_price', "订单价格");
+define('OGP_LANG_order', "订单");
+define('OGP_LANG_date', "日期");
+define('OGP_LANG_price', "价格");
+define('OGP_LANG_invoice', "发票");
+define('OGP_LANG_print_invoice', "打印发票");
+define('OGP_LANG_extend', "延长");
+?>
